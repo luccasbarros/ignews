4895f40a91f091ee9a3eb23b914f07f79ec6d4a3
Luccas Barros <luccas.candido@br.clara.net>
2021-04-14 10:08:02 -0300

    feat(api): github auth and faunadb integration

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,25 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import {FiX} from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/client'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  console.log(session)
+
+  return session ? (
+    <button
+    onClick={() => signOut()}
+    className={styles.signInButton} type="button">
       <FaGithub color="#04d361" />
-      Diego Fernandes
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+    onClick={() => signIn('github')}
+    className={styles.signInButton} type="button">
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import {GetServerSideProps} from 'next';
+import {GetStaticProps} from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
@@ -32,7 +32,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
   const price = await stripe.prices.retrieve('price_1IfjmrHzm51nD2LkEnAbDMsr')
 
   const product = {
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async() => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
   }
 } 
